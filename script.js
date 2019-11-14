8d98af69080b3261518070e4402d657bd52c46a8
CrazyScientist314 <AlexH3925@gmail.com>
2019-11-14 17:24:40 +0000

    added shop and button

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-$("#hi").click(function(){
-    $("body").append("<p>hello</p>");
+var point = 0;
+$("#point").text(point);
+$(".content").hide();
+
+$("#cookie").click(function(){
+    point = point + 1;
+    $("#point").text(point);
 });
+
+$("#openShop").click(function(){
+    if($(".content").is(":visible")){
+        $(".content").hide();
+    }else{
+        $(".content").show();
+    }
+});
+
+$(".item1").click(function(){
+
+})
+
