a9e24600ab22354618d19c46dabcd1d98db7fe02
CrazyScientist314 <AlexH3925@gmail.com>
2019-11-18 17:12:38 +0000

    progress and shop

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
 var point = 0;
 var increase = 1;
 var multiplier = 10;
+var progress = 0;
 
 $("#point").text(point);
 $(".content").hide();
 
 $("#cookie").click(function(){
     point = point + increase;
+    progress = progress + 1;
     $("#point").text(point);
     $("#cookie").css("width", "90px");
     setTimeout(function(){
         $("#cookie").css("width", "100px");
     }, 100);
+    $("#progress").attr("value", progress);
 });
 
+multiplyBy10(progress);
+
 $("#openShop").click(function(){
-    if($(".content").is(":visible")){
-        $(".content").hide();
-    }else{
-        $(".content").show();
-    }
+    $(".content").slideToggle();
 });
 
 $("#item1").click(function(){
     if (point >= multiplier) {
+        point = point - multiplier;
         increase = increase * 2;
-        multiplier = multiplier * 2;
+        multiplier = multiplier * increase;
+        $("#point").text(point);
         $("#item1Text").text(`Multiplier - ${multiplier} points`);
+    } else {
+        alert("Not enough points to purchase!")
     }
 });
+
+$("#item2").click(function(){
+    buy(point, 100);
+})
+
+$("#item3").click(function(){
+    buy(point, 120);
+})
 
 function buy(money, cost) {
     if (money >= cost) {
@@ -39,8 +52,17 @@ function buy(money, cost) {
     }
 }
 
-function changeSize() {
-    $("#cookie").css("width", "100px");
+function mutiplyBy10 (progressValue) {
+    if (progressValue >= 100) {
+        alert("Points x10 next 10 seconds");
+        setTimeout(function() {
+            $("#cookie").attr("id", "opCookie");
+            $("#opCookie").click(function(){
+                point = point + (10 * increase);
+            });
+        }, 10000);
+    }
+    progress = 0;
+    $("#progress").attr("value", progress);
+    $("#opCookie").attr("id", "cookie");
 }
-
-
